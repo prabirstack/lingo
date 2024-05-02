57a2a5bc66ec93d112c22e6ebdb149025378ca95
prabirstack <prabirsacademy@gmail.com>
2024-05-03 04:07:27 +0530

    stage 2: User progress completed

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,18 @@
+import { redirect } from 'next/navigation'
+import { getUserProgress } from '@/db/queries'
 import { FeedWrapper } from '@/components/feed-wrapper'
 import { StickyWrapper } from '@/components/sticky-wrapper'
 import { Header } from './header'
 import { UserProgress } from '@/components/user-progress'
 
-export default function LearnPage() {
+export default async function LearnPage() {
+  const userProgressData = getUserProgress()
+  const [userProgress] = await Promise.all([userProgressData])
+
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect('/courses')
+  }
+
   return (
     <div className='flex flex-row-reverse gap-[48px] px-6'>
       <StickyWrapper>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Nunito } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+import { Toaster } from '@/components/ui/sonner'
 import './globals.css'
 
 const font = Nunito({ subsets: ['latin'] })
@@ -17,9 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-    <html lang='en'>
-      <body className={font.className}>{children}</body>
-    </html>
+      <html lang='en'>
+        <body className={font.className}>
+          <Toaster />
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
   )
 }
